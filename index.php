91790dcd4875f0f5d2f76a593ee08b8ec4588ed2
OlaMyszkowska <112945105+OlaMyszkowska@users.noreply.github.com>
2023-01-31 18:34:32 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+        <nav class="grid-1">Książki</nav>
+        <nav class="grid-2">
+        <ul id="list">
+        <li>
+        <a href="Index.php"> Menu Główne</a>
+        </li>
+        <li>
+           <a href="AddBook.php">Dodaj Książkę</a> 
+        </li>
+        </ul>
+        </nav>
+        <main class="grid-4">
+     
+        </main>
+        <footer class="grid-3">Ola Myszkowska &copy</footer>
+        <script src="addbook.js"></script>
+</body>
+</html>
